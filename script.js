b56a7a6e747b500f5d840535c3287aa605c1d1e6
Zhialesem <zhialesem@gmail.com>
2023-01-27 15:13:45 +0300

    верстка 2

---
+++ script.js
@@ -0,0 +1,26 @@
+let addButton = container.querySelector('.form__submit-btn_action_add');
+addButton.addEventListener('click', addSong);
+
+// Находим форму в DOM
+let formElement = // Воспользуйтесь методом querySelector()
+    // Находим поля формы в DOM
+    let nameInput = // Воспользуйтесь инструментом .querySelector()
+        let jobInput = // Воспользуйтесь инструментом .querySelector()
+
+        // Обработчик «отправки» формы, хотя пока
+        // она никуда отправляться не будет
+        function handleFormSubmit(evt) {
+            evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+            // Так мы можем определить свою логику отправки.
+            // О том, как это делать, расскажем позже.
+
+            // Получите значение полей jobInput и nameInput из свойства value
+
+            // Выберите элементы, куда должны быть вставлены значения полей
+
+            // Вставьте новые значения с помощью textContent
+        }
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
