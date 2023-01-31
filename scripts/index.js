a4b80485f45e38808e931880422a7c8e25b020cc
Zhialesem <zhialesem@gmail.com>
2023-02-01 00:02:02 +0300

    finish

--- scripts/index.js
+++ scripts/index.js
@@ -1,21 +1,35 @@
 //popup open/ close
-let editButton = document.querySelector(".profile__btn-edit");
-let closeButton = document.querySelector(".popup__btn-close");
+let editButton = document.querySelector(".profile__btn-edit"); //ищем поле кнопки
+let closeButton = document.querySelector(".popup__btn-close"); //ищем поле кнопки
 let popup = document.querySelector(".popup");
 
-function toggleOpenPopup() {
+//read old data from html
+function readDataFromText() {                                        // функция записи из текста в попап
+    //name
+    let profile_Name = document.querySelector('.profile__name');  //ищем в документе поле имени
+    let popup_name = document.querySelector('.popup__name');   //ищем в попапе поле имени
+    popup_name.value = (profile_Name.textContent);;   //в попап переписываем значение поля имени из хтмл
+
+    //job
+    let profile_Job = document.querySelector('.profile__job');
+    let popup_job = document.querySelector('.popup__job');
+    popup_job.value = (profile_Job.textContent);
+};
+
+function toggleOpenPopup() {                                    //функция откр/закр попапа
     popup.classList.toggle('popup_opened');
 };
 
-let handleEditButtonClick = () => {
+let handleEditButtonClick = () => {                        //обработчик кнопки 
+    toggleOpenPopup();
+    readDataFromText()                                     //и вызов заполнения попапа из текста
+};
+
+let handleCloseButtonClick = () => {                         //обработчик кнопки
     toggleOpenPopup();
 };
 
-let handleCloseButtonClick = () => {
-    toggleOpenPopup();
-};
-
-let handleOverlyClick = (event) => {
+let handleOverlyClick = (event) => {                        //обработчик кнопки
     if (event.target === event.currentTarget) {
         toggleOpenPopup();
     }
@@ -25,15 +39,44 @@ editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
 popup.addEventListener('click', handleOverlyClick);
 
-//read data from html
-let saveButton = document.querySelector('.popup__btn-save');
 
-let profile_Name = document.querySelector('.profile__name');
-let profileName = (profile_Name.textContent);
+// Находим форму в DOM
+let formElement = document.querySelector(".popup__form");
+let nameInput = formElement.querySelector(".popup__name");
+let jobInput = formElement.querySelector(".popup__job");
 
-console.log(profileName)
+// Обработчик «отправки» формы, хотя пока
+// она никуда отправляться не будет
 
-let popup_name = document.querySelector('.popup__name');
-popup_name.value = profileName;
-console.log(popup_name.value)
+
+function handleFormSubmit(evt) {
+    evt.preventDefault();
+
+
+    // Эта строчка отменяет стандартную отправку формы.
+    // Так мы можем определить свою логику отправки.
+    // О том, как это делать, расскажем позже.
+
+    // Получите значение полей jobInput и nameInput из свойства value
+
+    profile_Name = document.querySelector('.profile__name'); // Выберите элементы, куда должны быть вставлены значения полей
+    profile_Job = document.querySelector('.profile__job');
+
+    // Вставьте новые значения с помощью textContent
+
+    profile_Name.textContent = nameInput.value;
+    profile_Job.textContent = jobInput.value;
+
+
+    toggleOpenPopup();                                      //закрываем попап
+}
+
+// Прикрепляем обработчик к форме:
+// он будет следить за событием “submit” - «отправка»
+formElement.addEventListener('submit', handleFormSubmit);
+
+
+
+
+
 
