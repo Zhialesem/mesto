01e5d3c449afe4fb764bb2bafe76d0e9f4081e80
Zhialesem <zhialesem@gmail.com>
2023-02-15 11:05:29 +0300

    сделать автозаполнение

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,29 @@
+const initialCards = [
+    {
+        name: 'Архыз',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+    },
+    {
+        name: 'Челябинская область',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+    },
+    {
+        name: 'Иваново',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+    },
+    {
+        name: 'Камчатка',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+    },
+    {
+        name: 'Холмогорский район',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+    },
+    {
+        name: 'Байкал',
+        link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+    }
+]; 
 //popup open/ close
 let editButton = document.querySelector(".profile__btn-edit"); //ищем поле кнопки
 let closeButton = document.querySelector(".popup__btn-close"); //ищем поле кнопки
@@ -44,22 +70,21 @@ let handleOverlyClick = (event) => {                        //обработчи
 };
 
 
-logoField.addEventListener('mouseover', toggleLogo);          //слушатель наезда и съезда с элемента
+logoField.addEventListener('mouseover', toggleLogo);          //слушатель наезда и съезда с элемента лого
 logoField.addEventListener('mouseout', toggleLogo);
+
 editButton.addEventListener('click', handleEditButtonClick);
 closeButton.addEventListener('click', handleCloseButtonClick);
 popup.addEventListener('click', handleOverlyClick);
 
 
 // Находим форму в DOM
-let formElement = document.querySelector(".popup__form");
-let nameInput = formElement.querySelector(".popup__input_type_name");
-let jobInput = formElement.querySelector(".popup__input_type_job");
+const formElement = document.querySelector(".popup__form");
+const nameInput = formElement.querySelector(".popup__input_type_name");
+const jobInput = formElement.querySelector(".popup__input_type_job");
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-
-
 function handleFormSubmit(evt) {
     evt.preventDefault();
 
@@ -85,6 +110,35 @@ function handleFormSubmit(evt) {
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 formElement.addEventListener('submit', handleFormSubmit);
+
+const elementWrapper = document.querySelector('.elements'); //контейнер фотографий
+const form = document.querySelector('popup-new-item'); //попап добавления картинки
+const template = document.getElementById('element');  //темплейт
+
+
+
+const getElement = (caption, image) => {
+    const newElement = template.content.cloneNode(true);             //создаем из темплейта
+    const newElementCaption = newElement.querySelector('.element__caption');//ищем в новом элементе заголовок
+    newElementCaption.textContent = caption;                                 //передаем значение в поле
+    const newElementImage = newElement.querySelector('.element__img');//поле картинки
+    newElementImage.src = image;
+    return newElement;
+}
+
+
+
+const renderItem = (wrap, caption, image) => {
+    wrap.append(getElement(caption, image));
+}
+
+initialCards.forEach((card) => {//пробегаем по начальному массиву
+    const caption = card.name;
+    const image = card.link;
+    renderItem(elementWrapper, caption, image);
+
+})
+
 
 
 
