6e76d181b54b724319fab6cdc0a32efc16073a2d
Zhialesem <zhialesem@gmail.com>
2023-02-16 18:49:18 +0300

    добавить функцию новой карточки

--- scripts/index.js
+++ scripts/index.js
@@ -79,36 +79,28 @@ popups.forEach((popup) => {
 }
 );
 
-// Находим форму в DOM
-const formElement = document.querySelector(".popup__form");
-const nameInput = document.querySelector(".popup__input_type_name");
-const jobInput = document.querySelector(".popup__input_type_job");
 
-
-function handleFormSubmit(evt) { // Обработчик «отправки» формы, хотя пока  она никуда отправляться не будет
+// обработчик сабмита Edit profile
+const formEditProfile = document.querySelector(".popup__edit-form");
+function handleEditFormSubmit(evt) {
     evt.preventDefault();    // Эта строчка отменяет стандартную отправку формы.
-    profileName = document.querySelector('.profile__name'); // Выберите элементы, куда должны быть вставлены значения полей
-    profileJob = document.querySelector('.profile__job');
-    profileName.textContent = nameInput.value;  // Вставьте новые значения с помощью textContent
-    profileJob.textContent = jobInput.value; // Получите значение полей jobInput и nameInput из свойства value
+    const fieldFirstStr = document.querySelector('.profile__name'); // Выберите элементы, куда должны быть вставлены значения полей
+    const fieldSecondStr = document.querySelector('.profile__job');
+    fieldFirstStr.textContent = document.querySelector(".popup__input_type_name").value;  // Вставьте новые значения с помощью textContent
+    fieldSecondStr.textContent = document.querySelector(".popup__input_type_job").value; // Получите значение полей jobInput и nameInput из свойства value
     ClosePopup(popupEditProfile);                                      //закрываем попап
 }
-
-// Прикрепляем обработчик к форме:
-// он будет следить за событием “submit” - «отправка»
-formElement.addEventListener('submit', handleFormSubmit);
+formEditProfile.addEventListener('submit', handleEditFormSubmit);
 
 const elementWrapper = document.querySelector('.elements'); //контейнер фотографий
 const form = document.querySelector('popup-new-item'); //попап добавления картинки
 const template = document.getElementById('element');  //темплейт
 
+
+//удаление карточки
 const handleDeconstr = (evt) => {
     evt.target.closest('.element').remove();
 };
-const handleLiked = (evt) => {
-    evt.target.closest('.element__btn-like').classList.add('element__btn-like_active');
-};
-
 elementWrapper.addEventListener('click', (evt) => {
     console.log(evt.target);
     if (evt.target.classList.contains('element__btn-del')) {
@@ -118,6 +110,13 @@ elementWrapper.addEventListener('click', (evt) => {
     }
 });
 
+
+//обработка лайков
+const handleLiked = (evt) => {
+    evt.target.closest('.element__btn-like').classList.toggle('element__btn-like_active');
+};
+
+//подготовка новой карточки
 const getElement = (caption, image) => {
     const newElement = template.content.cloneNode(true);             //создаем из темплейта
     const newElementCaption = newElement.querySelector('.element__caption');//ищем в новом элементе заголовок
@@ -127,22 +126,49 @@ const getElement = (caption, image) => {
     return newElement;
 }
 
-
-//добавка карточки. initial - маркер для добавления карты из коробки
+//отрисовка карточки. initial - маркер для добавления карты из коробки
 const renderItem = (wrap, caption, image, initial) => {
     if (initial) {
         wrap.append(getElement(caption, image));  //в конец, если из коробки
-        initial = false;
-    }                    //добавление карточки
+    }
     else wrap.prepend(getElement(caption, image));         //в начало
 }
 
 //подготовка карточек из коробки
 initialCards.forEach((card) => {//пробегаем по начальному массиву
-    const initial = true;
-    renderItem(elementWrapper, card.name, card.link, initial);
-
+    renderItem(elementWrapper, card.name, card.link, initial = true);
 })
+
+
+function handleEditFormSubmit(evt) {
+    evt.preventDefault();    // Эта строчка отменяет стандартную отправку формы.
+    const fieldFirstStr = document.querySelector('.profile__name'); // Выберите элементы, куда должны быть вставлены значения полей
+    const fieldSecondStr = document.querySelector('.profile__job');
+    fieldFirstStr.textContent = document.querySelector(".popup__input_type_name").value;  // Вставьте новые значения с помощью textContent
+    fieldSecondStr.textContent = document.querySelector(".popup__input_type_job").value; // Получите значение полей jobInput и nameInput из свойства value
+    ClosePopup(popupEditProfile);                                      //закрываем попап
+}
+
+//обработка submit попапа новой карточки
+ const formAddCard = document.querySelector(".popup__add-form");
+
+
+   
+    formAddCard.addEventListener('submit', (evt) => {
+        const cap = formAddCard.querySelector(".popup__input_type_name");
+         const caption = formAddCard.querySelector(".popup__input_type_name").value;
+          const im = formAddCard.querySelector(".popup__input_type_crc");
+    const image = formAddCard.querySelector(".popup__input_type_crc").value;
+       
+        evt.preventDefault();
+        renderItem(elementWrapper, caption, image, initial = false);
+        // evt.target.title.value = '';
+
+        ClosePopup(popupNewCard);
+    })
+
+
+
 
 //logo затемнение
 const logoField = document.querySelector(".header__logo"); //ищем поле кнопки
